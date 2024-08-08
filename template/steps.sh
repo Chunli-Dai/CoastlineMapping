01b5087ca7825d25e7bcd9607f0332dbbecff898
Chunli Dai <dai.56@buckeyemail.osu.edu>
2024-08-08 17:00:10 -0400

    Update steps.sh

--- template/steps.sh
+++ template/steps.sh
@@ -1,24 +1,6 @@
 #/bin/sh -f
 
-#mapformats was moved out of codec2
-
-tilelist=('arcticdem_01_iceland' 'arcticdem_02_greenland_southeast' 'arcticdem_03_greenland_southwest' 'arcticdem_04_greenland_central' 'arcticdem_05_greenland_northeast' 'arcticdem_06_greenland_northwest' 'arcticdem_07_canada_ellesmere' 'arcticdem_08_canada_baffin' 'arcticdem_09_canada_victoria' 'arcticdem_10_canada_north_mainland' 'arcticdem_11_canada_north_hudson' 'arcticdem_12_canada_south_nwt' 'arcticdem_14_svalbard' 'arcticdem_15_russia_novaya_zemlya' 'arcticdem_18_russia_cherskly' 'arcticdem_19_russia_magadanskaya' 'arcticdem_20_russia_kamchatka' 'arcticdem_21_russia_yakutiya_east' 'arcticdem_22_russia_central_east' 'arcticdem_23_russia_yakutiya_west' 'arcticdem_24_russia_central_west' 'arcticdem_25_russia_norilsk' 'arcticdem_26_russia_petersburg' 'arcticdem_27_russia_murmansk' 'arcticdem_28_scandinavia' 'arcticdem_29_russia_franz_josef' 'arcticdem_30_russia_siberian_islands' 'arcticdem_31_alaska_south' 'arcticdem_34_alaska_north')
-
-nlist=${#tilelist[@]}
-echo Total number of tiles: $nlist.
-
-echo ${tilelist[*]}
-
-#check ~/codec2/readme.txt for steps before this.
-
-for (( i=14; i<=$nlist; i++ ))
-do
-dir=${tilelist[$i-1]}
-echo $i $dir
-cd /u/cdai/work/runalltiles/
-mkdir $dir
-
-#dir=arcticdem_12_canada_south_nwt
+dir=arcticdem_12_canada_south_nwt
 cd $dir
 cp /u/cdai/template/* .   # warning: need to change the directory
 #cp ../rema_07_qml_ne/*.m . ; sed -i 's/rema_07_qml_ne/'$dir'/g' constant.m
@@ -46,7 +28,4 @@ qsub job_nov.pbs #run Tilemain_nov.m to update the arcticdem_nov.tif and get mat
 #bundle all jobs
 nohup ./run_change_group_pgc_par.sh > outrun1 &
 
-cd ../
-#exit
-done
 
